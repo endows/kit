987ba3bcdb22b50a6ff354236adaf087235dabf6
遠藤孝志 <takashi@endoutahinoiMac.lan>
2015-06-03 18:40:00 +0900

    first

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo curl https://raw.githubusercontent.com/endows/kit/master/kit > /usr/bin/kit
-sudo chmod +x /usr/bin/kit
+curl https://raw.githubusercontent.com/endows/kit/master/kit > /usr/bin/kit
+chmod +x /usr/bin/kit
